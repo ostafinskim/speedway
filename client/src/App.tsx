0867d9f8b39647855924b0d9a407c0df6f7b21e6
Mirek Ostafinski <ostafinskim@gmail.com>
2022-06-17 21:17:39 +0200

    feat: basic router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import Landing from './pages/Landing';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
-  return <h1>Initial files 🎉</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/login' element={<h1>Login</h1>} />
+          <Route path='/register' element={<h1>Register</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
